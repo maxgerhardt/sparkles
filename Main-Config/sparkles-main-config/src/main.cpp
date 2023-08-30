fe4f2f9c8ab9862f8d5a20f24a46da1f761355b5
Julian Finn <julian@phinn.de>
2023-08-30 16:33:36 +0200

    inbetween / next up rewrite for esp32

--- Main-Config/sparkles-main-config/src/main.cpp
+++ Main-Config/sparkles-main-config/src/main.cpp
@@ -10,43 +10,55 @@
 #define SEND_ADDRESS 1
 #define CALIBRATE 2
 #define RETRIEVE_TIMES 3
+#define END_CALIBRATION 4
+#define ASSIGN_ID 5
+#define RETURN 6
 volatile uint32_t lastMillis = 0;
 ESP8266Timer ITimer;
 uint32_t timerCounter = 0;
-int mode = 0;
+int mode = CALIBRATE;
 uint8_t clientAddresses[300][6];
 int clientAddressCounter = 0;
 int clientAddressTimerCounter = 0;
 int retrieverCounter = 0;
 
-uint8_t broadcastAddress[] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
+//uint8_t broadcastAddress[] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
+//DC:4F:22:7D:D2:7D
+uint8_t broadcastAddress[] = {0xDC, 0x4F, 0x22, 0x7D, 0xD2, 0x7D};
 typedef struct struct_message {
-  uint8_t a[6];
-  int b;
-  float c;
-  String d;
-  bool e;
+  uint8_t address[6];
+  int counter;
+  float timing;
+  int mode;
+  bool truefalse;
 } struct_message;
+struct_message myData;
 
 void IRAM_ATTR TimerHandler()
 {
-  myData.b = timerCounter;
+  myData.counter = timerCounter;
+  Serial.println(mode);
+
+  Serial.print("SUCCESS ");
+  Serial.println(ESP_NOW_SEND_SUCCESS );
+  Serial.print("FAIL ");
+  Serial.println(ESP_NOW_SEND_FAIL);
   if (mode == SEND_ADDRESS) {
     if (clientAddressTimerCounter !=0 && timerCounter > clientAddressTimerCounter+ 2) {
       mode = CALIBRATE;
     }
-    myData.d = "SEND_ADDRESS";
+    myData.mode = SEND_ADDRESS;
     esp_now_send(broadcastAddress, (uint8_t *) &myData, sizeof(myData));
   }
   else if (mode == CALIBRATE ) {
-    myData.d = "TIMER";
+    myData.mode = CALIBRATE;
     esp_now_send(broadcastAddress, (uint8_t *) &myData, sizeof(myData));
   }
   else if (mode == RETRIEVE_TIMES) {
     //figure out a good algorithm to retrieve the runtimes from the individual clients
     //keep in mind that each client needs to be polled individually and we need a different kind of banddwith than "per second" for this
     //esp_now_send(broadcastAddress, (uint8_t *) &myData, sizeof(myData));
-
+    //ESP_NOW_SEND_SUCCESS in sending callback function if the data is received successfully on the MAC layer. Otherwise, it will return ESP_NOW_SEND_FAIL. 
   }
 
   timerCounter++;
@@ -60,7 +72,7 @@ void OnDataRecv(uint8_t * mac, uint8_t  *incomingData, uint8_t len) {
     memcpy(&myData, incomingData, len);
   }
   if (mode == SEND_ADDRESS) {
-    memcpy(&clientAddresses[clientAddressCounter], myData.a, 6);
+    memcpy(&clientAddresses[clientAddressCounter], myData.address, 6);
     clientAddressTimerCounter = timerCounter;
     clientAddressCounter++;
   }
@@ -76,9 +88,6 @@ void OnDataSent(uint8_t *mac_addr, uint8_t sendStatus) {
     Serial.println("Delivery fail");
   }
 }
-
-struct_message myData;
-
 
 
 void setup() {
@@ -96,11 +105,11 @@ void setup() {
     Serial.println("Error initializing ESP-NOW");
     return;
   }
-  WiFi.macAddress(myData.a); 
+  WiFi.macAddress(myData.address); 
   esp_now_set_self_role(ESP_NOW_ROLE_CONTROLLER);
   esp_now_register_send_cb(OnDataSent);
   esp_now_add_peer(broadcastAddress, ESP_NOW_ROLE_SLAVE, 1, NULL, 0);
-  mode = SEND_ADDRESS;
+  mode = CALIBRATE;
 }
 
 void loop() {
--- client-config/src/main.cpp
+++ client-config/src/main.cpp
@@ -4,17 +4,20 @@
 #include <DNSServer.h>
 #include <WiFiManager.h>
 #include <espnow.h>
-#define MODE_CONFIG 1
-#define MODE_RTRV_VALUES 2
-#define MODE_RUN 3
+#define SEND_ADDRESS 1
+#define CALIBRATE 2
+#define RETRIEVE_TIMES 3
+#define END_CALIBRATION 4
+#define ASSIGN_ID 5
+#define RETURN 6
 int mode = 0;
 uint8_t broadcastAddress[] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
 typedef struct struct_message {
-    uint8_t a[6];
-    int b;
-    float c;
-    String d;
-    bool e;
+  uint8_t address[6];
+  int counter;
+  float timing;
+  int mode;
+  bool truefalse;
 } struct_message;
 
 struct_message myData;
@@ -26,44 +29,51 @@ int myID = 0;
 
 void sendTimes(int index) {
   struct_message returnData;
-  returnData.d = "RETURN";
-  returnData.b = index;
-  returnData.c = times[index];
+  returnData.mode = RETURN;
+  returnData.counter = index;
+  returnData.timing = times[index];
   esp_now_send(mainAddress, (uint8_t *) &returnData, sizeof(returnData));
 }
 
 void OnDataRecv(uint8_t * mac, uint8_t  *incomingData, uint8_t len) {
+  Serial.println("Received");
   if (len == sizeof(myData)) {
     memcpy(&myData, incomingData, sizeof(myData));
   }
-
-  if (myData.d == "TIMER") {
-    if (mode != MODE_CONFIG) {
-      mode = MODE_CONFIG;
+  Serial.println(myData.mode);
+  if (myData.mode == CALIBRATE) {
+    if (mode != CALIBRATE) {
+      mode = CALIBRATE;
     }
-    if (timerCounter != myData.b){
-    timerCounter = myData.b;
+    if (timerCounter != myData.counter){
+    timerCounter = myData.counter;
     }
-    memcpy (&mainAddress, myData.a, sizeof(myData.a));
+    memcpy (&mainAddress, myData.address, sizeof(myData.address));
+    String out;
+    WiFi.macAddress(broadcastAddress);
+  for (int i = 0; i<sizeof(broadcastAddress);i++){
+      out = out+":"+String(broadcastAddress[i]);    
+    }
+    Serial.println(out);
   }
-  if (myData.d == "END_TIMER") {
+  if (myData.mode == END_CALIBRATION) {
     mode = 0;
   }
-  if (myData.d == "SEND_TIMES") {
-    if (mode != MODE_RTRV_VALUES) {
-      mode = MODE_RTRV_VALUES;
+  if (myData.mode == RETRIEVE_TIMES) {
+    if (mode != RETRIEVE_TIMES) {
+      mode = RETRIEVE_TIMES;
     }
-    sendTimes(myData.b);
+    sendTimes(myData.counter);
   }
-  if (myData.d == "SEND_ADDRESS") { 
+  if (myData.mode == SEND_ADDRESS) { 
     if (myID == 0) {
       struct_message returnData;
-      WiFi.macAddress(returnData.a);
-      esp_now_send(myData.a, (uint8_t *) &returnData, sizeof(returnData));
+      WiFi.macAddress(returnData.address);
+      esp_now_send(myData.address, (uint8_t *) &returnData, sizeof(returnData));
     }
   }
-  if (myData.d == "ASSIGN_ID") {
-    myID = myData.b;
+  if (myData.mode == ASSIGN_ID) {
+    myID = myData.counter;
   }
 
 }
@@ -85,7 +95,7 @@ void setup() {
 }
 
 void loop() {
-  if (mode == MODE_CONFIG) {
+  if (mode == CALIBRATE) {
     //analogRead
   }
 }
