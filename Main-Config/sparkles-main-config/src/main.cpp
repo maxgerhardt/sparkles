ad32f1f7a15f9fb3705bc0a2b963f205d73b04a6
Julian Finn <julian@phinn.de>
2024-03-27 23:32:52 +0100

    zwischenstand

--- Main-Config/sparkles-main-config/src/main.cpp
+++ Main-Config/sparkles-main-config/src/main.cpp
@@ -1,21 +1,29 @@
 #include <Arduino.h>
 #include <esp_now.h>
 #include <WiFi.h>
-#include "ESP32TimerInterrupt.h"
+//#include "ESP32TimerInterrupt.h"
+//#include "driver/gptimer.h"
 #define TIMER_INTERVAL_MS       1000
 #define USING_TIM_DIV1 true
 
 #define HELLO 0
+#define ANNOUNCE 1
 #define WAIT_FOR_TIMER 1
 #define TIMER_CALIBRATION 2
-#define WAIT_FOR_CALIBRATE 3
+#define GOT_TIMER 3
 #define CALIBRATE 4
 #define ASK_CLAP_TIME 5
 #define SEND_CLAP_TIME 6
 #define ANIMATE 7
 #define NOCLAPFOUND -1
 int mode = HELLO;
+#define NUM_DEVICES 20;
+hw_timer_t * timer = NULL;
+int interruptCounter;  //for counting interrupt
+int totalInterruptCounter;   	//total interrupt counting
 
+//portMUX_TYPE timerMux = portMUX_INITIALIZER_UNLOCKED;
+ 
 
 //----------
 //wifi stuff
@@ -26,9 +34,10 @@ int mode = HELLO;
 //xiao address
 //uint8_t broadcastAddress[] = {0x34,0x85,0x18,0x5,0x88,0xb0};
 //CLIENT ESP32
-uint8_t broadcastAddress[] = {0x7c,0x87,0xce,0x2d,0xcf,0x98};
+uint8_t broadcastAddress[] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
 uint8_t emptyAddress[] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
 esp_now_peer_info_t peerInfo;
+uint8_t timerReceiver[] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
 
 //-------
 //message types
@@ -39,10 +48,12 @@ struct message_timer {
   uint16_t sendTime;
   uint16_t lastDelay;
 } timerMessage;
-typedef struct message_mode_change {
-  uint8_t messageType;
-  uint8_t mode;
-} modeChange;
+
+struct message_announce {
+  uint8_t messageType = ANNOUNCE;
+  uint16_t sendTime;
+  uint8_t address[6];
+} announceMessage;
 
 struct message_timer_received {
   uint8_t messageType = WAIT_FOR_TIMER;
@@ -50,38 +61,15 @@ struct message_timer_received {
   uint8_t timerOffset;
 } timerReceivedMessage;
 
-struct message_clap_time {
-  uint8_t messageType = SEND_CLAP_TIME;
-  int clapCounter;
-  int timerCounter;
-  int timeStamp;
-} claps[100];
-
-struct message_send_clap {
-  uint8_t messageType = ASK_CLAP_TIME;
-  uint8_t address[6];
-  uint8_t clapIndex;
-} sendClap ;
 struct message_address{
   uint8_t messageType = HELLO;
   uint8_t address[6];
 } addressMessage;
 
 
-
-struct client_address {
-  uint8_t address[6] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
-  int id;
-  float xLoc;
-  float yLoc;
-  float zLoc;
-} clientAddresses[200];
-
-
 //-----------
 //Timer config variables
 //-----------
-ESP32Timer ITimer(0);
 int msgSendTime;
 int msgArriveTime;
 int msgReceiveTime;
@@ -90,7 +78,7 @@ bool newMsgReceived = false;
 int timesince;
 int newMsgTime;
 int lastDelay = 0;
-uint32_t timerCounter = 0;
+int timerCounter = 0;
 
 //calibration
 int sensorValue;
@@ -102,83 +90,150 @@ int lastClap = 0;
 //receive addresses
 int addressCounter = 0;
 
-bool IRAM_ATTR TimerHandler(void * timerNo)
-{ 
+
+
+struct client_address {
+  uint8_t address[6] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
+  int id;
+  float xLoc;
+  float yLoc;
+  float zLoc;
+} clientAddresses[20];
+
+void printAddress(const uint8_t * mac_addr){
+  char macStr[18];
+  snprintf(macStr, sizeof(macStr), "%02x:%02x:%02x:%02x:%02x:%02x",
+           mac_addr[0], mac_addr[1], mac_addr[2], mac_addr[3], mac_addr[4], mac_addr[5]);
+  Serial.println(macStr);
+}
+
+
+void IRAM_ATTR onTimer()
+{   
+    msgSendTime = micros();
+    Serial.print("Counter in Timer");
+    Serial.print(timerCounter);
     timerCounter++;
+    if (timerCounter > 10000) {
+      Serial.println("resetting to 0");
+      timerCounter = 0;
+    }
     //wait for timer vs wait for calibrate
     if (mode == TIMER_CALIBRATION and timerCounter < 100) {
-      msgSendTime = micros();
       timerMessage.messageType = TIMER_CALIBRATION;
       timerMessage.sendTime = msgSendTime;
       timerMessage.counter = timerCounter;
       timerMessage.lastDelay = lastDelay;
-      esp_err_t result = esp_now_send(clientAddresses[addressCounter-1].address, (uint8_t *) &timerMessage, sizeof(timerMessage));
+      Serial.println("SENDING TIMER TO CLIENT");
+
+      esp_err_t result = esp_now_send(timerReceiver, (uint8_t *) &timerMessage, sizeof(timerMessage));
       newMsgSent = true;
     }
+    else {
+      announceMessage.sendTime = msgSendTime;
+      esp_err_t result = esp_now_send(broadcastAddress, (uint8_t *) &announceMessage, sizeof(announceMessage));
+    }
 
-  return true;
+  //return true;
 }
 
 
-void OnDataRecv(const uint8_t * mac, const uint8_t *incomingData, int lenn) {
+ 
+void addPeer(uint8_t * address) {
+  Serial.print("Adding peer");
+  printAddress(address);
+  memcpy(&peerInfo.peer_addr, address, 6);
+  if (esp_now_get_peer(peerInfo.peer_addr, &peerInfo) == ESP_OK) {
+    Serial.println("Found Peer");
+    return;
+  }
+  peerInfo.channel = 0;  
+  peerInfo.encrypt = false;
+    // Add peer        
+  if (esp_now_add_peer(&peerInfo) != ESP_OK){
+    Serial.println("Failed to add peer");
+    return;
+  }
+  else {
+    Serial.println("Added Peer");
+  }
+}
+
+void removePeer(uint8_t address[6]) {
+  if (esp_now_del_peer(address) != ESP_OK) {
+    Serial.println("coudln't delete peer");
+    return;
+  }
+}
+
+void  OnDataRecv(const esp_now_recv_info * mac, const uint8_t *incomingData, int len) {
   if (mode == HELLO) {
     if (incomingData[0] == HELLO) { 
+      Serial.println("received hello");
       memcpy(&addressMessage,incomingData,sizeof(addressMessage));
       for (int i = 0; i < sizeof(clientAddresses); i++) {
-        if (clientAddresses[i].address == emptyAddress) {
+        if (memcmp(&clientAddresses[i].address, &emptyAddress, 6) == true) {
+          Serial.println("Adding Address");
           memcpy(&clientAddresses[i].address, addressMessage.address, sizeof(addressMessage.address));
+          memcpy(timerReceiver, addressMessage.address, sizeof(addressMessage.address));
+          addPeer(timerReceiver);
           addressCounter++;
           break;
         }
         else if (clientAddresses[i].address == addressMessage.address) {
           break;
         }
+        mode = TIMER_CALIBRATION;
+
       }
     }
     else if (incomingData[0] == WAIT_FOR_TIMER) {
       //check if incomingdata[1] can be in accordance with current Timer
+      Serial.println("WAIT FOR TIMER");
       mode = TIMER_CALIBRATION;
+      memcpy(&timerReceiver, addressMessage.address, sizeof(addressMessage.address));
+      addPeer(timerReceiver);
     }
-    else if (incomingData[0] == WAIT_FOR_CALIBRATE) {
+    else if (incomingData[0] == GOT_TIMER) {
+      removePeer(timerReceiver);
       mode = HELLO;
     }
-    else if (incomingData[0] == NOCLAPFOUND) {
-      Serial.print("No Clap Found for index");
-      Serial.println(incomingData[7]);
-    }
-    else if (incomingData[0] == ASK_CLAP_TIME) {
-      // how to save this?
-      // use current address counter
     }
     //how to actually store the clap data?
 
 
   }
 
-}
 
-void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t sendStatus) {
+
+
+void  OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t sendStatus) {
+  //das geht natürlich nur wenn der richtige message status 
+  Serial.println("-------");
+  Serial.println("message sent");
+  printAddress(mac_addr);
+  
   if (sendStatus == ESP_NOW_SEND_SUCCESS) {
+    Serial.println("Success");
     msgArriveTime = micros();
     lastDelay = msgArriveTime-msgSendTime;
   }
   else {
     msgArriveTime = 0;
   }
+  Serial.println("-------");
 }
 
 
 void setup() {
   Serial.begin(115200);
-  
-  if (ITimer.attachInterruptInterval(TIMER_INTERVAL_MS * 1000, TimerHandler))
-  {
-    Serial.print(F("Starting  ITimer OK, millis() = ")); 
-    Serial.println(millis());
-  }
-  else 
-    Serial.println(F("Can't set ITimer correctly. Select another freq. or interval")); 
-  
+
+  timer = timerBegin(1000000);           	// timer 0, prescalar: 80, UP counting
+  timerAttachInterrupt(timer, &onTimer); 	// Attach interrupt
+  timerWrite(timer, 0);  		// Match value= 1000000 for 1 sec. delay.
+  timerStart(timer);   
+  timerAlarm(timer, 5000000, true, 0);
+
   WiFi.mode(WIFI_STA);
   if (esp_now_init() != ESP_OK) {
     Serial.println("Error initializing ESP-NOW");
@@ -196,21 +251,21 @@ void setup() {
   esp_now_register_send_cb(OnDataSent);
   esp_now_register_recv_cb(OnDataRecv);  
   //esp_now_add_peer(broadcastAddress, ESP_NOW_ROLE_SLAVE, 1, NULL, 0);
-  
-
+  WiFi.macAddress(announceMessage.address);
+  timerCounter = 0;
 }
 
 void loop() {
-  if (mode == CALIBRATE) {
-    sensorValue = analogRead(microphonePin);
-    if (sensorValue < 50 and millis() > lastClap+1000) {
-      claps[clapCounter].timerCounter = timerCounter;
-      claps[clapCounter].timeStamp = micros();
-      lastClap = millis();
-      clapCounter++;
-    }
-  }
-  else if (mode == ASK_CLAP_TIME) {
+//esp_err_t result = esp_now_send(broadcastAddress, (uint8_t *) &announceMessage, sizeof(announceMessage));
+Serial.println(".");
+printAddress(timerReceiver);
+Serial.print("Mode: ");
+Serial.println(mode);
+Serial.print("TimerCounter ");
+Serial.println(timerCounter);
+delay(10000);
+
+ /* else if (mode == ASK_CLAP_TIME) {
     //make sure claps actually correspond, in case one got lost or so?
     for (int i =0; i <= addressCounter; i++) {
       if (clientAddresses[i].address == emptyAddress) {
@@ -225,7 +280,7 @@ void loop() {
         }
       }
     }
-  }
+  }*/
 
 }
 
--- client-config/src/main.cpp
+++ client-config/src/main.cpp
@@ -1,27 +1,37 @@
 #include <Arduino.h>
 #include <esp_now.h>
 #include <WiFi.h>
-#include "ESP32TimerInterrupt.h"
+//#include "ESP32TimerInterrupt.h"
 #define TIMER_INTERVAL_MS       1000
 #define USING_TIM_DIV1 true
 
+#define HELLO 0
+#define ANNOUNCE 1
 #define WAIT_FOR_TIMER 1
 #define TIMER_CALIBRATION 2
-#define PRINT_MODE -1
-#define HELLO 0
-#define WAIT_FOR_CALIBRATE 3
+#define GOT_TIMER 3
 #define CALIBRATE 4
 #define ASK_CLAP_TIME 5
-#define SEND_CLAP_TIME 6 
+#define SEND_CLAP_TIME 6
+#define ANIMATE 7
 #define NOCLAPFOUND -1
+
 int mode;
+boolean from_head = true;
+int foo = 0;
+
 
 
 //Network
 //uint8_t broadcastAddress[] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
-uint8_t broadcastAddress[] = {0xB4,0xE6,0x2D,0xE9,0x3C,0x21};
+uint8_t broadcastAddress[] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF };
+uint8_t hostAddress[] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
+uint8_t hostdAddress[] = {0x7c, 0x87, 0xce, 0x2d, 0xcf, 0x98 };
 uint8_t myAddress[6];
 esp_now_peer_info_t peerInfo;
+
+esp_now_peer_num_t peerNum;
+
 
 
 //Variables for Time Offset
@@ -32,13 +42,19 @@ struct message_timer {
   uint16_t lastDelay;
 } timerMessage, oldMessage;
 
+struct message_announce {
+  uint8_t messageType = ANNOUNCE;
+  uint16_t sendTime;
+  uint8_t address[6];
+} announceMessage;
+
 struct message_mode {
   uint8_t messageType;
   uint8_t mode;
 } modeChange;
 
 struct message_timer_received {
-  uint8_t messageType = WAIT_FOR_CALIBRATE;
+  uint8_t messageType = GOT_TIMER;
   uint8_t address[6];
   uint8_t timerOffset;
 } timerReceivedMessage;
@@ -69,7 +85,6 @@ struct no_clap_found {
 
 
 
-
 //timer stuff
 int timeOffset;
 int messageArriveTime;
@@ -93,41 +108,48 @@ int addressSending = 0;
 
 
 //timer stuff
-ESP32Timer ITimer(0);
+//ESP32Timer ITimer(0);
 uint32_t timerCounter = 0;
 
-
-bool IRAM_ATTR TimerHandler(void * timerNo)
-{ 
-  timerCounter++;
-  return true;
+void printAddress(const uint8_t * mac_addr){
+  char macStr[18];
+  snprintf(macStr, sizeof(macStr), "%02x:%02x:%02x:%02x:%02x:%02x",
+           mac_addr[0], mac_addr[1], mac_addr[2], mac_addr[3], mac_addr[4], mac_addr[5]);
+  Serial.print(macStr);
 }
 
+void addPeer(uint8_t * address) {
+  memcpy(&peerInfo.peer_addr, address, 6);
+  if (esp_now_get_peer(peerInfo.peer_addr, &peerInfo) == ESP_OK) {
+    return;
+  }
+  peerInfo.channel = 0;  
+  peerInfo.encrypt = false;
+    // Add peer        
+  if (esp_now_add_peer(&peerInfo) != ESP_OK){
+    Serial.println("Failed to add peer");
+    return;
+  }
+}
 
 void sendAddress() {
   esp_now_send(broadcastAddress, (uint8_t *) &addressMessage, sizeof(addressMessage));
 }
 
-void sendClapTime(int clapIndex) {
-  for (int i = 0; i <= clapCounter; i++) {
-    if (claps[i].timerCounter < clapCounter+2 or claps[i].timerCounter > clapCounter-2) { 
-      esp_now_send(broadcastAddress, (uint8_t *) &claps[i], sizeof(claps[i]));
-      break;
-    }
-  }
-  esp_now_send(broadcastAddress, (uint8_t *) &noClapFound, sizeof(noClapFound));
-
-}
 
 void receiveTimer(int messageArriveTime) {
   //wenn die letzte message maximal 300 mikrosekunden abweicht und der letzte delay auch nicht mehr als 1500ms her war, dann muss die msg korrekt sein
+  Serial.print ("receiving timer");
+  Serial.println(messageArriveTime);
   if (abs(messageArriveTime - lastTime-1000000) < 300 and abs(timerMessage.lastDelay) <1500) {
     //damit hab ich den zeitoffset.. 
     // if zeit - timeoffset % 1000 = 0: blink
     timeOffset = lastTime-oldMessage.sendTime;
     WiFi.macAddress(timerReceivedMessage.address);
     timerReceivedMessage.timerOffset = timeOffset;
-    esp_now_send(broadcastAddress,(uint8_t *) &timerReceivedMessage, sizeof(timerReceivedMessage) );
+    Serial.print("Sent: ");
+    Serial.println(esp_now_send(broadcastAddress,(uint8_t *) &timerReceivedMessage, sizeof(timerReceivedMessage) ));
+
   }
   else {
     lastTime = messageArriveTime;
@@ -136,22 +158,33 @@ void receiveTimer(int messageArriveTime) {
 }
 
 
-void OnDataRecv(const uint8_t * mac, const uint8_t *incomingData, int len) {
+
+
+
+
+void OnDataRecv(const esp_now_recv_info * mac, const uint8_t *incomingData, int len) {
+  Serial.print ("Received  ");
+  Serial.println(incomingData[0]);
+  memcpy(hostAddress, mac->src_addr, sizeof(hostAddress));
   switch (incomingData[0]) {
+    case ANNOUNCE: 
+    {
+      Serial.println("ANNOUNCE");
+      addPeer(hostAddress);
+      memcpy(&announceMessage, incomingData, sizeof(announceMessage));
+      esp_now_send(hostAddress, (uint8_t*) &addressMessage, sizeof(addressMessage));
+      mode=TIMER_CALIBRATION;
+      
+    }
+      break;
     case TIMER_CALIBRATION:  
+    {
+      Serial.println("TIMER CALIBRATION MESSAGE REECEIVED");
       messageArriveTime = micros();
       memcpy(&timerMessage,incomingData,sizeof(timerMessage));
       receiveTimer(messageArriveTime);
+    }
       break;
-    case CALIBRATE: 
-      mode = incomingData[1];
-      break;
-    case ASK_CLAP_TIME: 
-      mode = ASK_CLAP_TIME;
-      memcpy(&sendClap,incomingData,sizeof(sendClap));
-      sendClapTime(sendClap.clapIndex);
-      break;
-
     default: 
       Serial.println("Data type not recognized");
   }
@@ -163,6 +196,9 @@ void OnDataRecv(const uint8_t * mac, const uint8_t *incomingData, int len) {
 }
 
 void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t sendStatus) {
+  Serial.print("SENT - Mode:");
+  Serial.println(mode);
+  
   if (mode == HELLO) {
     if (ESP_NOW_SEND_SUCCESS == true) {
       mode = WAIT_FOR_TIMER;
@@ -170,7 +206,7 @@ void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t sendStatus) {
   }
   else if (mode == TIMER_CALIBRATION) {
     if (ESP_NOW_SEND_SUCCESS == true) {
-      mode = WAIT_FOR_CALIBRATE;
+      mode = CALIBRATE;
     }
   else if (mode == ASK_CLAP_TIME) {
     if (ESP_NOW_SEND_SUCCESS == false) {
@@ -181,7 +217,7 @@ void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t sendStatus) {
     }
   }
     else {
-      esp_now_send(broadcastAddress,(uint8_t *) &timerReceivedMessage, sizeof(timerReceivedMessage) );
+      //esp_now_send(broadcastAddress,(uint8_t *) &timerReceivedMessage, sizeof(timerReceivedMessage) );
     }
   }
 
@@ -192,15 +228,6 @@ void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t sendStatus) {
 void setup() {
   // Initialize Serial Monitor
   Serial.begin(115200);
-  // start timer
-  if (ITimer.attachInterruptInterval(TIMER_INTERVAL_MS * 1000, TimerHandler))
-  {
-    Serial.print(F("Starting  ITimer OK, millis() = ")); 
-    Serial.println(millis());
-  }
-  else 
-    Serial.println(F("Can't set ITimer correctly. Select another freq. or interval")); 
-  
   // Set device as a Wi-Fi Station
   WiFi.mode(WIFI_STA);
   WiFi.macAddress(addressMessage.address);
@@ -221,21 +248,19 @@ void setup() {
   esp_now_register_recv_cb(OnDataRecv);  
   esp_now_register_send_cb(OnDataSent);
   mode = HELLO;
+   WiFi.macAddress(addressMessage.address);
+   esp_now_get_peer_num(&peerNum);
+
+     Serial.print("Number of Peers start: ");
+  Serial.println(peerNum.total_num);
+  delay(2000);
+  
 }
 
 void loop() {
-  if (mode == HELLO) { 
-    sendAddress();
-    delay(50);
-  }
-  if (mode == CALIBRATE) {
-    sensorValue = analogRead(microphonePin);
-    if (sensorValue < 50 and millis() > lastClap+1000) {
-      claps[clapCounter].timerCounter = timerCounter;
-      claps[clapCounter].timeStamp = micros();
-      lastClap = millis();
-      clapCounter++;
-  }
+  printAddress(addressMessage.address);
+  Serial.println("");
 
-}
-}
+  delay(3000);
+
+} 
