abad3a92a6315eef5886741dfabfc28bd283b186
Julian Finn <julian@phinn.de>
2024-04-15 22:27:22 +0200

    showing martin stuff

--- Main-Config/sparkles-main-config/src/stateMachine.h
+++ Main-Config/sparkles-main-config/src/stateMachine.h
@@ -1,9 +1,10 @@
+#ifndef MODE_MACHINE_H
+#define MODE_MACHINE_H
 #define MODE_INIT -1
 #define MODE_SEND_ANNOUNCE 0
 #define MODE_SENDING_TIMER 1
 #define MODE_CALIBRATE 4
 #define MODE_ANIMATE 7
-#ifndef MODE_MACHINE_H
-#define MODE_MACHINE_H
+
     class modeMachine {};
 #endif
